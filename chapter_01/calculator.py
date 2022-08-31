18d0ddb2ade11faed5d47aec71559b77a819fb4a
Dave Hausler <dave.hausler@gmail.com>
2022-08-31 08:37:09 -0400

    initial commit

---
+++ chapter_01/calculator.py
@@ -0,0 +1,20 @@
+first_term = input('Enter the first number: ')
+second_term = input('Enter the second number: ')
+
+result = int(first_term) + int(second_term)
+print(first_term + ' + ' + second_term + ' = ' + str(result))
+
+result = int(first_term) - int(second_term)
+print(first_term + ' - ' + second_term + ' = ' + str(result))
+
+result = int(first_term) * int(second_term)
+print(first_term + ' * ' + second_term + ' = ' + str(result))
+
+result = int(first_term) / int(second_term)
+print(first_term + ' / ' + second_term + ' = ' + str(result))
+
+result = int(first_term) ** int(second_term)
+print(first_term + ' ^ ' + second_term + ' = ' + str(result))
+
+result = int(first_term) % int(second_term)
+print(first_term + ' % ' + second_term + ' = ' + str(result))
